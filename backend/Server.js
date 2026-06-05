1a882fd7574069fcdf6f8bd7ea54129366ddb424
Muskan <shaikmuskan0512@gmail.com>
2026-06-05 18:23:05 +0530

    render port

--- backend/Server.js
+++ backend/Server.js
@@ -19,6 +19,8 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
